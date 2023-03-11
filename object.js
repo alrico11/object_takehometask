913ddafa6b43f143f42accc12fc0d804eacd38c6
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-12 06:37:28 +0700

    commit - fix object_ku

--- object.js
+++ object.js
@@ -1,18 +1,24 @@
 //TUGAS 1
 
 const object_ku = {
-    nama : "Alrico Rizki",
-    asal : "Semarang",
-    wa : 085290328542,
-    aktivitas : ["tidur","makan","belajar"],
+    
+    setObj : function(str1,str2,numb1,numb2,arrKu,isiArr){
+        object_ku.nama = str1,
+        object_ku.asal = str2,
+        object_ku.umur = numb1,
+        object_ku.wa = numb2,
+        object_ku[arrKu] = isiArr
+    },
     tampilObj : function () { 
         console.log(object_ku.nama)
         console.log(object_ku.asal)
+        console.log(object_ku.umur)
         console.log(object_ku.wa)
         console.log(object_ku.aktivitas)
     }
 }
 
+object_ku.setObj("Alrico Rizki Wibowo","Kota Semarang",21,085290328542,"aktivitas","makan\n")
 object_ku.tampilObj()
 
 const objhitung = {
